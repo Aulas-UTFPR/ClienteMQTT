7ff100a14514ba9d2ddfb492abe3b2530df0a0db
Michel Albonico <bsd.albonico@gmail.com>
2021-11-11 21:40:18 -0300

    Update Client.java

--- src/main/java/mqttclient/Client.java
+++ src/main/java/mqttclient/Client.java
@@ -63,45 +63,45 @@ public class Client {
 	
 	void subscribe() {
 		// Latch used for synchronizing b/w threads
-        final CountDownLatch latch = new CountDownLatch(1);
-        
-        // Callback - Anonymous inner-class for receiving messages
-        cli.setCallback(new MqttCallback() {
+		final CountDownLatch latch = new CountDownLatch(1);
 
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                System.out.println("\nMensagem recebida!" + 
-                        "\n\tTopico:   " + topic + 
-                        "\n\tMensagem: " + new String(message.getPayload()));
-                latch.countDown(); // unblock main thread
-            }
+		// Callback - Anonymous inner-class for receiving messages
+		cli.setCallback(new MqttCallback() {
 
-            public void connectionLost(Throwable cause) {
-                System.out.println("Conexao perdida!" + cause.getMessage());
-                latch.countDown();
-            }
+		    public void messageArrived(String topic, MqttMessage message) throws Exception {
+			System.out.println("\nMensagem recebida!" + 
+				"\n\tTopico:   " + topic + 
+				"\n\tMensagem: " + new String(message.getPayload()));
+			latch.countDown(); // unblock main thread
+		    }
 
-			public void deliveryComplete(IMqttDeliveryToken token) {
-				// TODO Auto-generated method stub
-				
+		    public void connectionLost(Throwable cause) {
+			System.out.println("Conexao perdida!" + cause.getMessage());
+			latch.countDown();
+		    }
+
+				public void deliveryComplete(IMqttDeliveryToken token) {
+					// TODO Auto-generated method stub
+
+				}
+		});
+
+	     // Subscribe client to the topic filter and a QoS level of 0
+		System.out.println("Inscrevendo-se no topico: " + topic);
+		try {
+				cli.subscribe(topic, 0);
+			} catch (MqttException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-        });
-        
-     // Subscribe client to the topic filter and a QoS level of 0
-        System.out.println("Inscrevendo-se no topico: " + topic);
-        try {
-			cli.subscribe(topic, 0);
-		} catch (MqttException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        System.out.println("Inscrito");
+		System.out.println("Inscrito");
 
-        // Wait for the message to be received
-        try {
-            latch.await(); // block here until message received, and latch will flip
-        } catch (InterruptedException e) {
-            System.out.println("Fui acordado!");
-        }
+		// Wait for the message to be received
+		try {
+		    latch.await(); // block here until message received, and latch will flip
+		} catch (InterruptedException e) {
+		    System.out.println("Fui acordado!");
+		}
         
 	}
 	
